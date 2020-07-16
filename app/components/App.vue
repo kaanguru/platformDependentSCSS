ce9148453c72e05e7e6c0e4af42c8889d19df6cd
netlesh <netlesh@mail.ru>
2020-07-16 18:56:27 +0300

    BismalllahirrahmanirRahmanirRahim

---
+++ app/components/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <Page>
+    <ActionBar>
+      <GridLayout width="100%" columns="auto, *">
+        <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0" />
+        <Label class="title" text="Welcome to NativeScript-Vue!" col="1" />
+      </GridLayout>
+    </ActionBar>
+
+    <RadSideDrawer ref="drawer">
+      <StackLayout ~drawerContent backgroundColor="#ffffff">
+        <Label class="drawer-header" text="Drawer" />
+
+        <Label class="drawer-item" text="Item 1" />
+        <Label class="drawer-item" text="Item 2" />
+        <Label class="drawer-item" text="Item 3" />
+      </StackLayout>
+
+      <GridLayout ~mainContent columns="*" rows="*">
+        <Label class="message" :text="msg" col="0" row="0" />
+      </GridLayout>
+    </RadSideDrawer>
+  </Page>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      msg: "Hello World!",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~@/test.css";
+</style>
